bb61d74387f286a18ccb84498b5bee04a6834134
nominik-spb <61251151+nominik-spb@users.noreply.github.com>
2025-03-09 23:29:27 +0300

    1

--- src/Book.java
+++ src/Book.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
-
 public class Book {
+    public String title;
+    public int releaseYear;
+    public String author;
+    public int pages;
+
+    public Book(String title, int releaseYear, String author, int pages) {
+        this.title = title;
+        this.releaseYear = releaseYear;
+        this.author = author;
+        this.pages = pages;
+    }
+
+    public boolean isBig() {
+        if (pages > 500) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean matches(String word) {
+        if (title.contains(word) || author.contains(word)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int estimatePrice() {
+        int price = 0;
+        price = (pages * 3) >= 250 ? pages * 3 : 250;
+        return price;
+    }
+
+    public String toString() {
+        return "Книга '" + title + "' (" + author + ", " + releaseYear + " год)";
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class Main {
+
+    public static void printBook(Book book) {
+        System.out.println("_________________________________________________");
+        System.out.println(book);
+        System.out.println("Это большая книга?: " + book.isBig());
+        System.out.println("Эта книга содержит слово 'сердце'?: " + book.matches("сердце"));
+        System.out.println("Сколько стоит эта книга? " + book.estimatePrice() + "руб.");
+    }
+
+    public static void main(String[] args) {
+        Book book1 = new Book("Собачье сердце", 1968, "Булгаков М.А.", 159);
+        Book book2 = new Book("Огниво", 1835, "Андерсен Г.Х.", 32);
+        Book book3 = new Book("Война и мир", 1865, "Толстой Л.Н.", 1408);
+
+        printBook(book1);
+        printBook(book2);
+        printBook(book3);
+    }
 }
